05122390045401b0e6a868c906594bef2121bf04
Milton Rodrigues <miltonrodrigues713@gmail.com>
2021-10-31 18:26:05 -0300

    Added enzyme to project
    sugar syntax

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -7,7 +7,9 @@ it("shows a comment box", () => {
 
   ReactDOM.render(<App />, div);
 
-  expect(div.innerHTML).toContain("Comment Box");
+  // expect(div.innerHTML).toContain("Comment Box");
+
+  expect(div).toHaveAnInstanceOf(CommentBox);
 
   ReactDOM.unmountComponentAtNode(div);
 });
@@ -16,4 +18,5 @@ it("shows a comment box", () => {
 /* 1- todo o bloco de código dentro da função global IT é fornecido pela lib JSDOM do Jest que emula o ambiente do navegador, necessário para a execução de códigos com react. Sendo assim ele cria uma instância de uma div em HTML, dentro somente do nosso terminal para podermos ter o resultado. */
 /* 2- ReactDOM olha pra dentro da DIV criada em APP  e verifica. */
 /* 3- Para garantir perfomance, em cada teste gerado o unmountCompomenteAtNode apaga a situação do componente da div, recuperando memória do computador. */
+/* instale o Enzyme API na versão mais atual do react -> yarn add enzyme enzyme-adapter-react-16 */
 //////////////////////////////////////////////////////////////////////////////////////////////////
