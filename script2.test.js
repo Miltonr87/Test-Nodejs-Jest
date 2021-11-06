4e1db597b2121718959b48a8200b9001cd4517b5
Milton Rodrigues <miltonrodrigues713@gmail.com>
2021-11-06 17:07:19 -0300

    Jest async

--- script2.test.js
+++ script2.test.js
@@ -18,5 +18,25 @@ it('calls swapi to get people on API with Promises', () => {
     })
 });
 
+///////////////////////////// USANDO O JEST EM ASSÍNCRONOS /////////////////////////////////////
+
+it('getPeople returns count and results', () => {
+    const mockFetch = jest.fn()
+        .mockReturnValue(Promise.resolve({
+            json: () => Promise.resolve({
+                count: 87,
+                results: [0,1,2,3,4,5] 
+            })
+    }))
+    expect.assertions(4) // procura garantir que os dois expects sejam chamados
+    return swapi.getPeoplePromise(mockFetch).then(data => {
+        expect(mockFetch.mock.calls.length).toBe(1)
+        // aqui acima testa o tamanho do array se for 1, se for 0 ele falha
+        expect(mockFetch).toBeCalledWith('https://swapi.py4e.com/api/people/')
+        expect(data.count).toEqual(87)
+        expect(data.results.length).toBeGreaterThan(5)
+    });
+})
+
 // algumas lógicas para montagem dos testes pode ser encontrado no repositório Jest-Cheat-Sheet
 
